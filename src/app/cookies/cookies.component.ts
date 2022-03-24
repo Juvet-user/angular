84c389db326fd017a75aa3ffc603a79fa97036d9
Tajo Juvet (TT/BDO-FR1) <taj2dy@bosch.com>
2022-03-24 12:23:02 +0100

    make better

--- src/app/cookies/cookies.component.ts
+++ src/app/cookies/cookies.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+
+declare var init_datalayer: any;
+declare var gtag: any;
+declare var init_ga: any;
+declare var remove_ga:any;
+
 @Component({
   selector: 'app-cookies',
   templateUrl: './cookies.component.html',
@@ -10,6 +16,11 @@ export class CookiesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    new init_datalayer();
+    new gtag();
+    new init_ga();
+    new remove_ga();
   }
 
 }
