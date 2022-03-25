e199960a413fab64aa597c1f402396ae51baf3bd
Tajo Juvet (TT/BDO-FR1) <taj2dy@bosch.com>
2022-03-25 11:01:21 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {path:"**", redirectTo:"cookies"},
-  {path:"", redirectTo:"cookies", pathMatch:"full"},
+  {path:"/", redirectTo:"cookies", pathMatch:"full"},
   {path:"cookies", component: CookiesComponent}
 ];
 
